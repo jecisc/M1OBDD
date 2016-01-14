e2d20f5db4eda4577692646f51acce8b6706f577
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-01-14 09:49:41 +0100

    correct it

---
+++ TP1/tp1.sql
@@ -0,0 +1,6 @@
+-- QUESTION 1
+-- See export question 1
+
+-- Question2
+
+-- On a 22504 lignes. 50 blocks qui est la plus petite unitée de mesure en oracle. 1block = 8Ko de base mais il peut monter à 32Ko. Le nombre de block dépend de l'histoire de la table.  En moyenne dans ces trois langues les mots font 10 caractères.
