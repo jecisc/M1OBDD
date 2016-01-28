90c986e766eaf11dce26e9d7e90e5d083479cd25
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-01-28 09:55:19 +0100

    uate TP3

--- TP3/tp3.sql
+++ TP3/tp3.sql
@@ -14,18 +14,47 @@ RETURN mot.mot;
 END genere_mot;*/
 
 /*
-CREATE OR REPLACE PROCEDURE acrotische(chaine IN VARCHAR2)
+create or replace PROCEDURE acrotische(chaine IN VARCHAR2)
 AS
 TYPE char_tab IS TABLE OF VARCHAR2(1) INDEX BY BINARY_INTEGER;
 tab char_tab;
 tmp VARCHAR2(1);
 resultat VARCHAR2(100) := '';
+v_start NUMBER;
+v_stop NUMBER;
 BEGIN
+v_start := DBMS_UTILITY.GET_TIME;
 FOR i IN 1 .. LENGTH(chaine) LOOP
 DBMS_OUTPUT.PUT_LINE(genere_mot(substr(chaine, i, 1)));
 END LOOP;
+v_stop := DBMS_UTILITY.GET_TIME;
+DBMS_OUTPUT.PUT_LINE(((v_stop-v_start)*10) || ' ms');
 END acrotische;
 */
 
 set serveroutput on 
 call acrotische('TEST');
+
+/*
+delete from adjectifs where mot = '0';
+delete from adjectifs where mot = '1';
+delete from adjectifs where mot = '(página anterior) (página siguiente)';*/
+
+DECLARE
+v_start NUMBER;
+v_stop NUMBER;
+test VARCHAR2(40);
+test2 VARCHAR2(40);
+BEGIN
+
+  v_start := DBMS_UTILITY.GET_TIME;
+  select min(mot), max(mot)  into test, test2 from adjectifs where lang = 'FR';
+  v_stop := DBMS_UTILITY.GET_TIME;
+  DBMS_OUTPUT.PUT_LINE(((v_stop-v_start)*10) || ' ms');
+
+END;
+
+select min(mot) from adjectifs where lang = 'FR';
+select max(mot) from adjectifs where lang = 'FR';
+select min(mot) from adjectifs where lang = 'FR';
+select min(mot), max(mot) from adjectifs where lang = 'FR';
