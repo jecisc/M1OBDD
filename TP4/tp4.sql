72a9f71b78a080102a77462edff399fdbb3801f2
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-02-05 16:25:33 +0100

    finish TP :)

--- TP4/tp4.sql
+++ TP4/tp4.sql
@@ -17,27 +17,26 @@ ALTER TABLE Acrostiches ADD
   constraint id_acro_pk PRIMARY KEY (id_acro);
   
 ALTER TABLE Mot_Composant ADD
-  constraint mot_comp_pk PRIMARY KEY (id_acro, mot);
+  constraint mot_comp_pk PRIMARY KEY (id_acro, mot, position);
 
 ALTER TABLE Mot_Composant ADD
   constraint id_acro_fk FOREIGN KEY (id_acro) REFERENCES Acrostiches(id_acro);
 
 -- Question 2
 
-create or replace FUNCTION ID_ACROSTICHE (LOCUTION IN VARCHAR2, LANG IN VARCHAR2) RETURN NUMBER AS 
+create or replace FUNCTION ID_ACROSTICHE (LOCU IN VARCHAR2, LANGU IN VARCHAR2) RETURN NUMBER AS 
   res number;
 BEGIN
   dbms_output.put_line('pre-caca');
   BEGIN
   SELECT ID_ACRO INTO res
   FROM ACROSTICHES
-  WHERE LOCUTION = ACROSTICHES.LOCUTION AND LANG = ACROSTICHES.LANG;
+  WHERE LOCU = ACROSTICHES.LOCUTION AND LANGU = ACROSTICHES.LANG;
   EXCEPTION
       WHEN NO_DATA_FOUND THEN
          dbms_output.put_line('caca');
-         res := calc_acro(LOCUTION, LANG);
+         res := calc_acro(LOCU, LANGU);
     END;
-  COMMIT;
   RETURN res;
 END ID_ACROSTICHE;
 
@@ -49,18 +48,18 @@ BEGIN
  RETURN 1;
 END;
 
-create or replace FUNCTION CALC_ACRO (LOCUTION IN VARCHAR2, LANG IN VARCHAR2 ) RETURN NUMBER AS 
+create or replace FUNCTION CALC_ACRO (LOCU IN VARCHAR2, LANGU IN VARCHAR2 ) RETURN NUMBER AS 
   id number;
   test varchar2(40);
 BEGIN
-  id := insert_acro(LOCUTION, LANG);
+  id := insert_acro(LOCU, LANGU);
   
   SELECT id_acro INTO id
   FROM Acrostiches
-  WHERE LOCUTION = Acrostiches.locution AND LANG = Acrostiches.lang;
+  WHERE LOCU = Acrostiches.locution AND LANGU = Acrostiches.lang;
   
-  FOR i IN 1 .. LENGTH(locution) LOOP
-     test := genere_mot_in_table(substr(locution, i, 1), i, LANG, id);
+  FOR i IN 1 .. LENGTH(LOCU) LOOP
+     test := genere_mot_in_table(substr(LOCU, i, 1), i, LANGU, id);
   END LOOP;
   
   RETURN id;
@@ -74,24 +73,25 @@ BEGIN
  RETURN 1;
 END;
 
-create or replace FUNCTION GENERE_MOT_IN_TABLE (LETTER IN VARCHAR2, POS IN NUMBER, LANG IN VARCHAR2, ID_ACRO IN NUMBER ) RETURN VARCHAR2 AS 
+create or replace FUNCTION GENERE_MOT_IN_TABLE (LETTER IN VARCHAR2, POS IN NUMBER, LANGU IN VARCHAR2, ID_ACR IN NUMBER ) RETURN VARCHAR2 AS 
   TYPE char_tab IS TABLE OF VARCHAR2(1) INDEX BY BINARY_INTEGER;
   expression VARCHAR2(30) := CONCAT('^',LETTER);
   CURSOR c_mots IS SELECT mot FROM adjectifs
-  WHERE  REGEXP_LIKE (mot, expression) AND adjectifs.lang = LANG
+  WHERE  REGEXP_LIKE (mot, expression) AND adjectifs.lang = LANGU
   ORDER BY DBMS_RANDOM.RANDOM;
   res c_mots%ROWTYPE;
-  exit number;
+  exitNumber number;
   BEGIN
     OPEN c_mots;
     FETCH c_mots INTO res;
-    exit:= insert_mot(id_acro, res.mot, POS);
+    exitNumber:= insert_mot(ID_ACR, res.mot, POS);
   RETURN res.mot;
 END GENERE_MOT_IN_TABLE;
 
 set serveroutput on;
-select ID_ACROSTICHE('TEST', 'FR') from dual;
+select ID_ACROSTICHE('Anticonstitutionnellement', 'FR') from dual;
 
-SELECT ID_ACRO
-  FROM ACROSTICHES
-  WHERE  ACROSTICHES.LOCUTION = 'TEST' AND ACROSTICHES.LANG  = 'FR';
+SELECT MOT
+  FROM MOT_COMPOSANT
+  WHERE  ID_ACRO= 3
+  ORDER BY position;
