cda00479fa8ee6cd2f509fd8a21352271f63b788
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-02-11 10:00:51 +0100

    tp5

---
+++ TP5/TP5.sql
@@ -0,0 +1,38 @@
+create synonym ferlicot.prenom for BOSSUT.prenom;
+
+-- Question 1
+
+select * from prenom;
+
+-- Les éléves sont dans l'ordre alphabetique car les données ont été rentrées dans l'ordre alphabétique.
+
+select prenom from prenom where genre = 'Garçon';
+
+set serveroutput on;
+declare
+type pren_type is table of VARCHAR2(50) index by binary_integer ;
+prenoms pren_type;
+x NUMBER;
+begin
+select prenom bulk collect into prenoms from prenom where prenom.genre = 'Garçon';
+for i in prenoms.first..prenoms.last loop
+   dbms_output.put_line(prenoms(i));
+   x := ID_ACROSTICHE(prenoms(i), 'FR');
+   x := ID_ACROSTICHE(prenoms(i), 'EN');
+end loop;
+  
+select prenom bulk collect into prenoms from prenom where prenom.genre = 'Fille';
+for i in prenoms.first..prenoms.last loop
+   x := ID_ACROSTICHE(prenoms(i), 'FR');
+   x := ID_ACROSTICHE(prenoms(i), 'ES');
+end loop;
+  
+select prenom bulk collect into prenoms from prenom where prenom.genre = 'Mixte';
+for i in prenoms.first..prenoms.last loop
+   x := ID_ACROSTICHE(prenoms(i), 'FR');
+   x := ID_ACROSTICHE(prenoms(i), 'EN');
+   x := ID_ACROSTICHE(prenoms(i), 'ES');
+end loop;
+end;
+
+-- Question 2
