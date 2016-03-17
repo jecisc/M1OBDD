5f8588d1056c8b9235777d0045bf50c0a678756a
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-03-17 09:52:01 +0100

    continu TP

--- TP7/src/Scantest.java
+++ TP7/src/Scantest.java
@@ -6,30 +6,40 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.sql.*;
+
 public class Scantest {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Connection connectOracle = null;
         try {
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            System.out.println("Password ?");
+            String pw = "genji12";
             connectOracle =
                     DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora",
-                            "ferlicot", (new BufferedReader(new InputStreamReader(System.in))).readLine());
-            connectOracle.setAutoCommit(true); }
-        catch (SQLException e) { System.out.println("Erreur de connection");}
+                            "ferlicot", pw);
+            connectOracle.setAutoCommit(true);
+        } catch (SQLException e) {
+            System.out.println("Erreur de connection");
+        }
         Statement s = connectOracle.createStatement();
-        String namefile = your_file_name;
+        String namefile = "text.txt";
         File f = new File(namefile);
         Scanner scan = new Scanner(f);
         int idoccur = 1;
         String mot;
-        while(scan.hasNext()) {
+        while (scan.hasNext()) {
             mot = scan.next();
-
-
-            s.executeUpdate(
-                    "insert into docs values(lower('"+mot+"'),"+idoccur+",'"+namefile+"')");
-            idoccur++;
-
+            if (mot.length() > 2) {
+                String request = "select count(*) from bossut.motsnoirs where '" + mot + "'= motsnoirs.mot";
+                ResultSet res = s.executeQuery(request);
+                res.next();
+                if (!res.getBoolean(1)) {
+                    s.executeUpdate(
+                            "insert into docs (mot, nieme, document) values(lower('" + mot + "')," + idoccur + ",'" + namefile + "')");
+                    System.out.println("Inserted: " + mot);
+                    idoccur++;
+                }
+            }
 
         }
         scan.close();
--- TP7/tp7.sql
+++ TP7/tp7.sql
@@ -20,3 +20,30 @@ set autotrace on;
 
 select libelle from huguet.adjectifs_p where trie like 'aab%' and libelle like 'v%';
 
+DROP TABLE docs;
+
+CREATE TABLE docs(
+  mot VARCHAR2(40),
+  nieme NUMBER(4),
+  document VARCHAR2(70),
+  autre VARCHAR2(10),
+  CONSTRAINT PK_docs PRIMARY KEY (nieme, document)
+) ORGANIZATION INDEX;
+
+
+set autotrace on;
+
+select * from docs where document = 'text.txt' and nieme = 2;
+
+select * from docs where document = 'text.txt';
+
+select * from docs where document = 'text.txt' order by nieme;
+
+CREATE INDEX INDEX_QUEST_6 ON DOCS (MOT, DOCUMENT);
+
+set autotrace on;
+
+select * from docs where document = 'text.txt'and mot = 'cigale';
+select * from docs where mot ='cigale';
+
+
